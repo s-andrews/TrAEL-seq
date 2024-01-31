fb5320f22faa07b7d06b013bacf1cc1221535d71
laurabiggins <laurabiggins10@gmail.com>
2024-01-31 14:58:57 +0000

    minor updates - tidy and annotate

--- TrAELseq_preprocessing_UMIplusBarcode.py
+++ TrAELseq_preprocessing_UMIplusBarcode.py
@@ -8,18 +8,26 @@ import re
 # This script removes the first 8bp of a read and adds the sequence to the readID. The quality information is discarded
 # In addition, the sample level barcode will be written into the filename (quality information is also discarded)
 
-# This is the expected structure of the FastQ files will be:
+# This is the expected structure of the FastQ files:
 
 # UMI (8bp)    //   sample-level barcode  4bp   //    PolyT     //      Insert
 
-# After moving the UMI and sample-level barcode sequences, the script looks for up to 3 T at the start of the sequence, and removes those.
+# After moving the UMI and sample-level barcode sequences, the script looks for up to 3 T at the start of the 
+# sequence (original position 13), and removes those. 
 # Sequences with more than 3 Ts at the 5' end are clipped a maximum of 3 TTT
+# If no T is present at position 13, that base is still removed. We have previously identified that the quality 
+# at that position is often low, so we trim that one base to help with mapping later on.
+
+# In addition to splitting the sequences by 4bp barcode, there are also T and noT files, dependent on the 
+# presence of a T at position 13.
+# Therefore this script should produce 20 output fastq files for 1 input fastq file. 
+# 20 = (9 barcodes + 1 unassigned)*2 for T and noT.
 
 # Script last modified  11 January 2024 Laura Biggins
 
 polyT = {}         # storing the number of Poly Ts at the start of the read (after the UMI)
-fhs = {}           # storing the filehandles for all output files
-fhs_noT = {}           # storing the filehandles for all no T output files
+fhs = {}           # storing the filehandles for all T output files
+fhs_noT = {}       # storing the filehandles for all no T output files
 
 def submain():
 	
@@ -38,8 +46,6 @@ def submain():
 
 def main(filename):
 
-	expected_count   = 0
-	unexpected_count = 0   
 	count = 0              # total sequence count
 	
 	print (f"Reading file: >{filename}<")
@@ -65,8 +71,6 @@ def main(filename):
 			if count%500000 == 0:
 				print (f"Processed {count} reads so far")
 
-			# print (f"{readID}\n{seq}\n{line3}\n{qual}\n")
-		
 			## STEP 1: Remove UMI and write it into the read ID
 
 			# These are the expected in-line codes:
@@ -76,11 +80,7 @@ def main(filename):
 			sampleBarcode = seq[8:12:]
 			rest      = seq[12::]
 			qual_rest = qual[12::]
-			# print (f"sequence: {seq}\numi:  {barcode}\nsample-level barcode:    {sampleBarcode}\nrest:             {rest}\n")
 			readID += f':{sampleBarcode}:{barcode}'
-
-			#print (f"{readID}\n{rest}\n{line3}\n{qual_rest}\n")
-			#sleep(1)
 
 			## STEP 2: Now we need to find a number of PolyTs at the start
 
@@ -91,9 +91,8 @@ def main(filename):
 			if m is None:
 				print ("m is none")
 				# Using what we aleady have if the read does not have T(s) at the start
-				#new_rest = rest
-				#new_rest_qual = qual_rest
-				# we still want to remove a base even if it wasn't called as a T. The quality scores are low at this position as all the reads should have a T here.
+				# we still want to remove a base even if it wasn't called as a T. 
+				# The quality scores are low at this position as all the reads should have a T here.
 				new_rest      = rest[1::]
 				new_rest_qual = qual_rest[1::]
 								
@@ -120,22 +119,14 @@ def main(filename):
 				new_rest_qual = qual_rest[polyTlength::]
 				
 			readID = readID.replace(" ","_") # this is required for e.g. Bowtie2 to retain the last part of the read ID (= the UMI sequence)
-			
-			# print ("\n".join([readID, new_rest, line3, new_rest_qual]))
 
-			# currently indexes 1, 3, 4, 5, 6, 7, 8, 9
-            # Dec 2022 Adding in 2
+			# currently indexes 1, 2, 3, 4, 5, 6, 7, 8, 9
 			if sampleBarcode == "AGTC" or sampleBarcode == "GACT" or sampleBarcode == "CTTG" or sampleBarcode == "TCGA" or sampleBarcode == "AAGG" or sampleBarcode == "TTCC"  or sampleBarcode == "GTGC" or sampleBarcode == "GCCA" or sampleBarcode == "GATG":
-			#if sampleBarcode == "TTCC"  or sampleBarcode == "GTGC" or sampleBarcode == "GCCA":
-			# print (f"Expected: {sampleBarcode}")
 				if m is None:
-					print ("m is still none")
 					fhs_noT[sampleBarcode].write (("\n".join([readID, new_rest, line3, new_rest_qual]) + "\n").encode())
 				else:
 					fhs[sampleBarcode].write (("\n".join([readID, new_rest, line3, new_rest_qual]) + "\n").encode())
 			else:
-				# print (f"Unexpected/Unwanted")
-				
 				if m is None:
 					fhs_noT["unassigned"].write (("\n".join([readID, new_rest, line3, new_rest_qual]) + "\n").encode())
 				else:
@@ -144,8 +135,6 @@ def main(filename):
 				if sampleBarcode not in faithless_barcodes.keys():
 					faithless_barcodes[sampleBarcode] = 0
 				faithless_barcodes[sampleBarcode] += 1
-				# pass
-			# sleep(1)
 			
 	
 	close_filehandles()
@@ -188,7 +177,6 @@ def make_out_filehandle(sample_name,filename,TnoT):
 	print (filename)
 	m = p.findall(filename)
 	sample = m[0][0]
-	#ending = m[0][1]
 	ending = f"{TnoT}_{m[0][1]}"
 
 	new_filenames = []
@@ -225,7 +213,6 @@ def make_out_filehandle(sample_name,filename,TnoT):
 	new_filenames.append(f"{new_filename_10}:unassigned")
 
 	for new_fh in new_filenames:
-		#open_filehandles(new_fh.split(":")[1], new_fh.split(":")[0])
 		open_filehandles(new_fh.split(":")[1], new_fh.split(":")[0], TnoT)
 
 def open_filehandles(sample_level_barcode, fname, TnoT):
